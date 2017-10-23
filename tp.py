031f5c315cf23799cd9eb7e7101e0ed3dac79f5b
Mario Staykov <mstaykov@novarumdx.com>
2017-10-23 11:39:10 +0100

    Added subtle date printing

--- tp.py
+++ tp.py
@@ -4,15 +4,20 @@ import datetime as dt
 import sys
 from time import sleep
 
-def printHour(color):
-	sys.stdout.write(">>> ")
+def printWithStyle(mode):
+	'''Defines style and prints when called for an hour/date'''
 	now = dt.datetime.now()
-	print(colored("%0.2d:%0.2d" % (now.hour, now.minute), color))
+	if mode == "hour":
+		print(colored("%0.2d:%0.2d" % (now.hour, now.minute), "cyan"))
+	elif mode == "date":
+		print(colored("    %d/%d/%d" % (now.day, now.month, now.year), "green", attrs=['dark']))
 
 def clearScreen():
 	'''Clears screen'''
 	print("\033[H\033[J")
 
-printHour("cyan")
+sys.stdout.write(">>> ")
+printWithStyle("hour")
+printWithStyle("date")
 sleep(2)
 clearScreen()
